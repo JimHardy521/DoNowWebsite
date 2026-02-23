a9d41ea08fefa1d3a47e930f68bb1a64be56aa60
JimHardy <61353568+JimHardy521@users.noreply.github.com>
2026-02-23 20:48:55 +0000

    Add files via upload

--- common.js
+++ common.js
@@ -89,3 +89,13 @@ function createNavBar(currentPage) {
     window.location.href = "searchResults.html";
   });
 }
+
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
